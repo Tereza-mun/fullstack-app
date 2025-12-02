c4013328010821172f15c289f86336bd911628f0
terezamundleinova <tereza.mundleinova@gmail.com>
2025-12-02 10:42:13 +0100

    allow Netlify URL fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
