26f8dc95636b07893feb81ab24264d9b16bb7178
terezamundleinova <tereza.mundleinova@gmail.com>
2025-12-04 18:45:46 +0100

    Spinner added to indicate products loading

---
+++ frontend/src/components/atoms/Spinner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inline-block">
+        <div
+            :class="[
+                'border-[#1a1a2e]/10 border-t-[#1a1a2e] rounded-full animate-spin',
+                sizeClasses
+            ]"
+        ></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+    size?: 'sm' | 'md' | 'lg'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: 'md'
+})
+
+const sizeClasses = computed(() => {
+    switch (props.size) {
+        case 'sm':
+            return 'w-6 h-6 border-2'
+        case 'lg':
+            return 'w-[60px] h-[60px] border-4'
+        case 'md':
+        default:
+            return 'w-10 h-10 border-[3px]'
+    }
+})
+</script>
--- frontend/src/pages/products.vue
+++ frontend/src/pages/products.vue
@@ -4,11 +4,16 @@
 
         <ProductFilters v-model="filtersStore.filters" />
 
-        <div class="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-8">
-            <ProductItem 
-                v-for="product in filteredProducts" 
-                :key="product.id" 
-                :product="product" 
+        <div v-if="productsStore.loading" class="flex flex-col justify-center items-center py-20 gap-4">
+            <Spinner size="md" />
+            <p class="text-gray-600">Loading products...</p>
+        </div>
+
+        <div v-else class="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-8">
+            <ProductItem
+                v-for="product in filteredProducts"
+                :key="product.id"
+                :product="product"
             />
         </div>
     </Container>
@@ -22,6 +27,7 @@ import { useFiltersStore } from '../stores/filters'
 import ProductFilters from '../components/organisms/ProductFilters.vue'
 import ProductItem from '../components/molecules/ProductItem.vue'
 import Container from '../components/molecules/Container.vue'
+import Spinner from '../components/atoms/Spinner.vue'
 
 const productsStore = useProductsStore()
 const filtersStore = useFiltersStore()
