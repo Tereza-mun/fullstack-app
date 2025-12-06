ee649853500d16d48e822c44b3e6193be06bb780
terezamundleinova <tereza.mundleinova@gmail.com>
2025-12-06 09:47:08 +0100

    User icon added

---
+++ frontend/src/components/atoms/icons/User.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg
+        class="w-7 h-7"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        :stroke="stroke"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+        />
+    </svg>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    stroke?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    stroke: '#1a1a2e'
+});
+</script>
