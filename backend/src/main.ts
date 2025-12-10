848b160b96047ec29321752a8342fa443ce09ba5
terezamundleinova <tereza.mundleinova@gmail.com>
2025-12-10 12:11:40 +0100

    Backend fallback localhost fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
   app.useGlobalPipes(
